cb4528fefaa46c77cb11358823b312fae80c9dad
eIIka <vkalkatins@gmail.com>
2025-08-05 14:45:30 +0300

    Init project - Add README.md and LICENSE

---
+++ src/test/java/ua/ellka/medipack/MediPackApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.ellka.medipack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediPackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
